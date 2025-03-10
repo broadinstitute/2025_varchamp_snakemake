fb940716febeaf1699e111bb30ab023533c81bfe
Runxi Shen <shenrunxi@gmail.com>
2025-03-10 14:09:48 +0000

    Codes for plotting preliminary figures added

--- snakemake_pipeline/run_snakemake_pipeline.sh
+++ snakemake_pipeline/run_snakemake_pipeline.sh
@@ -3,6 +3,6 @@
 # nohup snakemake -s Snakefile_batch7 --cores all &> snakemake_batch7_final.log &
 # nohup snakemake -s Snakefile_batch8 --cores all &> snakemake_batch8_final.log &
 
-snakemake -s Snakefile_batch8 --cores all &> snakemake_batch8_test.log
+# snakemake -s Snakefile_batch8 --cores all &> snakemake_batch8_test.log
 # snakemake -s Snakefile_batch7_test --cores all &> snakemake_batch7_test.log
 
