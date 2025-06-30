ce5a13113c54bcfb8f349e06ec29eb657718d692
Runxi Shen <shenrunxi@gmail.com>
2025-06-30 01:33:39 +0000

    Adding more batch info

--- download_aws_cpg_data.sh
+++ download_aws_cpg_data.sh
@@ -10,17 +10,19 @@ AWS_WORKSPACE_PATH="s3://cellpainting-gallery/cpg0020-varchamp/broad/workspace"
 ## To be replaced by user's choices 
 CPG_IMG_PATH="./1.image_preprocess_qc/inputs/cpg_imgs" ## symbolic link to /data/shenrunx/igvf/varchamp/2021_09_01_VarChAMP_imgs
 SNAKEMAKE_INPUT_PATH="./2.snakemake_pipeline/inputs"
-BATCHES="2024_01_23_Batch_7 2024_02_06_Batch_8 2024_12_09_Batch_11 2024_12_09_Batch_12 2025_03_17_Batch_15 2025_03_17_Batch_16"
+# BATCHES="2024_01_23_Batch_7 2024_02_06_Batch_8"
+# 2024_12_09_Batch_11 2024_12_09_Batch_12 2025_03_17_Batch_15 2025_03_17_Batch_16"
+BATCHES="2025_06_10_Batch_18 2025_06_10_Batch_19"
 
 ## create directories
 # mkdir -p $CPG_IMG_PATH
 # mkdir -p $SNAKEMAKE_INPUT_PATH/single_cell_profiles
-mkdir -p $SNAKEMAKE_INPUT_PATH/metadata/platemaps
+# mkdir -p $SNAKEMAKE_INPUT_PATH/metadata/platemaps
 
 for batch_id in $BATCHES;
 do
     ## download the raw img data
-    # aws s3 sync --no-sign-request "$AWS_IMG_PATH/$batch_id/images" $CPG_IMG_PATH/$batch_id/images
+    #aws s3 sync --no-sign-request "$AWS_IMG_PATH/$batch_id/images" $CPG_IMG_PATH/$batch_id/images
     # aws s3 sync --no-sign-request \
     #     "$AWS_ANALYSIS_PATH/$batch_id" \
     #     "$CPG_IMG_PATH/$batch_id/analysis" \
@@ -33,6 +35,6 @@ do
         # --dry-run
 
     ## download the CellProfiler single-cell profiles and metadata for snakemake analysis pipeline
-    # aws s3 sync --no-sign-request --exclude "*.csv" "$AWS_WORKSPACE_PATH/backend/$batch_id" $SNAKEMAKE_INPUT_PATH/single_cell_profiles/$batch_id
+    #aws s3 sync --no-sign-request --exclude "*.csv" "$AWS_WORKSPACE_PATH/backend/$batch_id" $SNAKEMAKE_INPUT_PATH/single_cell_profiles/$batch_id
     aws s3 sync --no-sign-request "$AWS_WORKSPACE_PATH/metadata/platemaps/$batch_id" $SNAKEMAKE_INPUT_PATH/metadata/platemaps/$batch_id
 done
