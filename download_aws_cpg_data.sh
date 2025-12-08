7cac4b4ea006ec7d690de44cf264a3a394504395
Runxi Shen <shenrunxi@gmail.com>
2025-12-08 01:39:56 +0000

    Updated platemaps by re-downloading them from aws cpg for batch 7,8,11-16 after allele sequence confirmation

--- download_aws_cpg_data.sh
+++ download_aws_cpg_data.sh
@@ -11,10 +11,11 @@ AWS_ANALYSIS_PATH="s3://cellpainting-gallery/cpg0020-varchamp/broad/workspace/an
 ## To be replaced by user's choices 
 CPG_IMG_PATH="./1.image_preprocess_qc/inputs/cpg_imgs" ## symbolic link to /data/shenrunx/igvf/varchamp/2021_09_01_VarChAMP_imgs
 SNAKEMAKE_INPUT_PATH="./2.snakemake_pipeline/inputs"
-# BATCHES="2024_01_23_Batch_7 2024_02_06_Batch_8"
-# 2024_12_09_Batch_11 2024_12_09_Batch_12 2025_03_17_Batch_15 2025_03_17_Batch_16"
+BATCHES="2024_01_23_Batch_7 2024_02_06_Batch_8 2024_12_09_Batch_11 2024_12_09_Batch_12 2025_01_27_Batch_13 2025_01_28_Batch_14 2025_03_17_Batch_15 2025_03_17_Batch_16"
+
+## Private batches
 # BATCHES="2025_06_10_Batch_18 2025_06_10_Batch_19"
-BATCHES="2025_05_23_Batch_17"
+# BATCHES="2025_05_23_Batch_17"
 
 ## create directories
 # mkdir -p $CPG_IMG_PATH
@@ -39,5 +40,10 @@ do
     ## download the CellProfiler single-cell profiles and metadata for snakemake analysis pipeline
     # aws s3 sync --no-sign-request --exclude "*.csv" "$AWS_WORKSPACE_PATH/backend/$batch_id/2025_04_01_B17A9R1" $SNAKEMAKE_INPUT_PATH/single_cell_profiles/$batch_id/2025_04_01_B17A9R1
     # aws s3 sync --no-sign-request --exclude "*.csv" "$AWS_WORKSPACE_PATH/backend/$batch_id/2025_04_21_B17A9R2_media_change" $SNAKEMAKE_INPUT_PATH/single_cell_profiles/$batch_id/2025_04_21_B17A9R2_media_change
-    aws s3 sync --no-sign-request "$AWS_WORKSPACE_PATH/metadata/platemaps/$batch_id" $SNAKEMAKE_INPUT_PATH/metadata/platemaps/$batch_id
+    aws s3 sync --no-sign-request \
+        "$AWS_WORKSPACE_PATH/metadata/platemaps/$batch_id/platemap" \
+        $SNAKEMAKE_INPUT_PATH/metadata/platemaps/$batch_id/platemap \
+        --exclude "*" \
+        --include "*.txt" \
+        --exclude "*-checkpoint.txt" #\ --dryrun
 done
